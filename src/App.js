7bdf9eeaf914cdb204cb3b692edfdb1599984976
Kevin M <crisstian2345@gmail.com>
2023-05-27 04:39:47 -0500

    remove: some comment

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import { useEffect } from "react";
 export default function App() {
   useEffect(() => {
     const onKeyDown = (e) => {
-      console.log(e);
       const btns = document.querySelectorAll('.btn');
       for(const btn of btns){ //try whit Array.from -- .find()
         if(btn.textContent === e.key || btn.dataset.key === e.key){
           e.preventDefault();
           btn.click();
-          // console.log(btn);
           btn.classList.add('active');
           setTimeout(() => {
             btn.classList.remove('active');
-          }, 300);
+          }, 250);
           break;
         }
       }
